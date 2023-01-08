c095af3cb11c9fd03908686c89c55b9d0fd7c83f
KavetiRohith <rohitkaveti@gmail.com>
2023-01-08 13:34:07 +0530

    use flags to set the TCP bind address

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/KavetiRohith/go-cache/cache"
 	"github.com/KavetiRohith/go-cache/server"
 )
 
+var listenAddr = flag.String("addr", ":3000", "Set the TCP bind address")
+
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 	opts := server.ServerOpts{
-		ListenAddr: ":3000",
+		ListenAddr: *listenAddr,
 	}
 
 	server := server.NewServer(opts, cache.New())
